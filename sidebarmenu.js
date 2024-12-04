0ebce7ca8046b2a73b0bb51ca91e87c2fbc28c40
eli-r-goldman <dgoldman1@student.waubonsee.edu>
2024-12-03 21:53:26 -0600

    Add files via upload

---
+++ sidebarmenu.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+  document.getElementById("sidebarMenu").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeSidebar() {
+  document.getElementById("sidebarMenu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+} 
